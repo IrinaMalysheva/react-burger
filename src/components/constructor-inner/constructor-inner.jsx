d18e9ff78fc89807391d065acde16d72b12e88f7
IrinaMalysheva <irina.malysheva@onlyoffice.com>
2022-02-25 21:49:24 +0300

    Add BurgerConstructor component.

---
+++ src/components/constructor-inner/constructor-inner.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { menuItemPropTypes } from '../../utils/constants';
+import constructorInnerStyles from './constructor-inner.module.css';
+import ScrollableSection from '../scrollable-section/scrollable-section';
+import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+function ConstructorInner(props) {
+    return (
+        <ScrollableSection styleProp={{ height: '464px', margin: "16px 0" }}>
+            <ul className={constructorInnerStyles.innerContainer}>
+                {
+                    props.data.map((item) => {
+                        return (item.type != "bun") &&
+                            <li className={`mr-2 mb-4 ${constructorInnerStyles.constructorItem}`} key={item._id}>
+                                <DragIcon type="primary" />
+                                <ConstructorElement
+                                    text={item.name}
+                                    price={item.price}
+                                    thumbnail={item.image}
+                                />
+                            </li>
+                    })
+                }
+            </ul>
+        </ScrollableSection>
+    )
+};
+
+ConstructorInner.propTypes = {
+    data: PropTypes.arrayOf(menuItemPropTypes).isRequired
+}
+
+export default ConstructorInner;
