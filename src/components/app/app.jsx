edf3aadfe2e16bb5c7f32a7a2784c7f3bd60173b
IrinaMalysheva <irina.malysheva@onlyoffice.com>
2022-02-28 09:43:48 +0300

    Get data from API.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
-import data from '../../utils/data';
+import { React, useState, useEffect } from 'react';
+//import data from '../../utils/data';
 import appStyles from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 
-function App() {
+const App = () => {
+  const API_URL = "https://norma.nomoreparties.space/api/ingredients";
+
+  const [state, setState] = useState(
+    {
+      data: null,
+    });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const resJson = await response.json();
+        setState({ data: resJson.data });
+      }
+      catch (e) {
+        console.log(e)
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={`App p-10 ${appStyles.App}`}>
       <AppHeader />
-      <main className="flexContainerJcCenter">
-        <BurgerIngredients data={data}/>
-        <BurgerConstructor data={data} />
-      </main>
+      {state.data &&
+        <main className="flexContainerJcCenter">
+          <BurgerIngredients data={state.data} />
+          <BurgerConstructor data={state.data} />
+        </main>
+      }
     </div>
   );
 }
