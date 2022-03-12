80ef0730e9be8748f7f3c1225d7baf808081d2e9
IrinaMalysheva <irina.malysheva@onlyoffice.com>
2022-03-12 14:17:17 +0300

    Add "@redux-devtools/extension.

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { compose, createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from '../src/services/reducers';
 import './index.css';
 import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
 
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose; 
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-const store = createStore(rootReducer, enhancer); 
+// const composeEnhancers =
+//   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+//     : compose; 
+// const enhancer = composeEnhancers(applyMiddleware(thunk));
+// const store = createStore(rootReducer, enhancer); 
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
