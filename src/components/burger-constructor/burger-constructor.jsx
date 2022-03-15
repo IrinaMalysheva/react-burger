6a6baf5daeb32920b36c8a75aa9bdefb99a7c0e8
IrinaMalysheva <irina.malysheva@onlyoffice.com>
2022-03-16 01:38:03 +0300

    Calculate totalPrice from Redux store value, get rid of TotalPriceContext.

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,8 +1,7 @@
-import { useReducer, useEffect } from 'react';
+import { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useDrop } from "react-dnd";
 import { v4 as uuidv4 } from 'uuid';
-import { TotalPriceContext } from '../../utils/burgerDataContext';
 import burgerConstructorStyles from './burger-constructor.module.css';
 import ConstructorInner from '../constructor-inner/constructor-inner';
 import { Button, ConstructorElement, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
@@ -18,23 +17,13 @@ function BurgerConstructor() {
     const constructorFillingIngredients = useSelector(state => state.ingredientsOrder.constructorFillingIngredients);
     const orderRequest = useSelector((store) => store.ingredientsOrder.orderRequest);
     const dispatch = useDispatch();
-    const initialTotalPrice = { totalPrice: 0 };
-    const [totalPrice, totalPriceDispatch] = useReducer(reducer, initialTotalPrice);
 
-    function reducer(state, action) {
-        switch (action.type) {
-            case "add":
-                return { totalPrice: state.totalPrice + action.reducerPrice };
-            case "remove":
-                return { totalPrice: state.totalPrice - action.reducerPrice };
-            default:
-                throw new Error(`Wrong type of action: ${action.type}`);
-        }
-    }
-
-    // useEffect(() => {
-    //     bunTopBottom && totalPriceDispatch({ type: 'add', reducerPrice: bunTopBottom.price * 2 });
-    // }, [bunTopBottom]);
+    const totalPrice = useMemo(() => {
+        let interimPrice = constructorBun ? constructorBun.price * 2 : 0;
+        return (constructorFillingIngredients)
+            ? constructorFillingIngredients?.reduce((sum, item) => sum + item.price, interimPrice)
+            : interimPrice
+    }, [constructorBun, constructorFillingIngredients] );
 
     const handleClick = (e) => {
         dispatch({ type: OPEN_MODAL });
@@ -77,11 +66,9 @@ function BurgerConstructor() {
                         />
                     </section>
                 }
-                <TotalPriceContext.Provider value={totalPriceDispatch}>
-                    {constructorFillingIngredients &&
-                        <ConstructorInner data={constructorFillingIngredients} totalPriceDispatch={totalPriceDispatch} onDelete={handleRemove} />
-                    }
-                </TotalPriceContext.Provider>
+                {constructorFillingIngredients &&
+                    <ConstructorInner data={constructorFillingIngredients} onDelete={handleRemove} />
+                }
                 {constructorBun &&
                     <section className="ml-8">
                         <ConstructorElement
@@ -96,7 +83,7 @@ function BurgerConstructor() {
             </div>
             <div className={`mt-10 ${burgerConstructorStyles.finalPart}`}>
                 <p className={`mr-10`}>
-                    <span className={`text text_type_digits-medium ${burgerConstructorStyles.pricePart}`}>{totalPrice.totalPrice}</span>
+                    <span className={`text text_type_digits-medium ${burgerConstructorStyles.pricePart}`}>{totalPrice}</span>
                     <CurrencyIcon type="primary" />
                 </p>
                 <Button type="primary" size="large" onClick={handleClick} disabled={isDisabled}>
--- src/components/constructor-inner-element/constructor-inner-element.jsx
+++ src/components/constructor-inner-element/constructor-inner-element.jsx
@@ -1,17 +1,9 @@
-import { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { menuItemPropTypes } from '../../utils/constants';
-import { TotalPriceContext } from '../../utils/burgerDataContext';
 import constructorInnerElementStyles from './constructor-inner-element.module.css';
 import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 function ConstructorInnerElement({ itemData, id, onDelete }) {
-    const totalPriceDispatch = useContext(TotalPriceContext);
-
-    useEffect(() => {
-        totalPriceDispatch({ type: 'add', reducerPrice: itemData.price });
-    }, []);
-
     return (
         <li className={`mr-2 mb-4 ${constructorInnerElementStyles.constructorItem}`} id={id}>
             <DragIcon type="primary" />
