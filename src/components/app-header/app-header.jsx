4fb007222e5afc1d17234aef1897ff68b14059dd
IrinaMalysheva <irina.malysheva@onlyoffice.com>
2022-03-28 00:34:21 +0300

    Edit header.

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import appHeaderStyles from './app-header.module.css';
-import { Icons, BurgerIcon, ListIcon, ProfileIcon, Logo, Typography } from '@ya.praktikum/react-developer-burger-ui-components';
+import { BurgerIcon, ListIcon, ProfileIcon, Logo, Typography } from '@ya.praktikum/react-developer-burger-ui-components';
 
-function AppHeader(props) {
+function AppHeader() {
     return (
-        <header className={`ml-5 mr-4 mt-4 ${appHeaderStyles.header}`}>
+        <header className={appHeaderStyles.header}>
             <nav className={appHeaderStyles.nav}>
                 <div className={appHeaderStyles.innerNav}>
                     <a className={`mb-4 mt-4 mr-2 pl-5 pr-5 pb-4 pt-4 text text_type_main-default ${appHeaderStyles.headerLink} ${appHeaderStyles.active}`} href="">
@@ -16,7 +16,7 @@ function AppHeader(props) {
                         <span className=''>Лента заказов</span>
                     </a>
                 </div>
-                <a className={appHeaderStyles.headerLink} href=""><Logo /></a>
+                <Link className={appHeaderStyles.headerLink} to='/'><Logo /></Link>
                 <div className={`${appHeaderStyles.innerNav} ${appHeaderStyles.alignedRight}`}>
                     <a className={`mb-4 mt-4 pl-5 pr-5 pb-4 pt-4 text text_type_main-default ${appHeaderStyles.headerLink} ${appHeaderStyles.inactive}`} href="">
                         <span className={`mr-2 text_type_main-default ${appHeaderStyles.iconWrapper}`}><ProfileIcon type="secondary" /></span>
